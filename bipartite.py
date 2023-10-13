be68d388fade6685562b175808587aacca7f37a0
Tiffany <mejia9tiffany@gmail.com>
2023-10-12 23:17:05 -0700

    Add all conditions for all problems

--- bipartite.py
+++ bipartite.py
@@ -16,21 +16,21 @@ class Problem3:
         self.visited = {}
 
 
-    def isBipartite(self, graph: list, n: int = 0) -> bool:
+    def isBipartite(self, graph: list, n: int) -> bool:
         """
             Function determines if given inut graph is a bipirite graph
             
             @param graph: (list) list[list[int]] graph to check
-            @param n: (int) test node to start search, default is 0
+            @param n: (int) len of vertices 
 
             @return: (bool) Is or is not Bipartite
         """
-        if self.visited.get(n, None) is None:
-            self.visited[n] = True # initialize start of graph iteration 
-        try:   
-            for neighbor in graph[n]:
-                if not self.search(graph, neighbor, True):
-                    return False
+        try:
+
+            for node in range(n):
+                if node == 0:
+                    if not self.search(graph, node, True):
+                        return False
             return True
         except IndexError: 
             print("Invalid test node given. Give valid node to test within given graph range.")
@@ -48,18 +48,18 @@ class Problem3:
         if self.visited.get(node, None) is not None:
             return self.visited.get(node) != before_color
         self.visited[node] = not before_color
-     
-        neighbors  = graph[node]
+        neighbors = graph[node]
         for n in neighbors:
             if not self.search(graph, n, not(before_color)):
                 return False
         return True
 
+# Main used to test algorithm
 if __name__ == "__main__":
     test_class = Problem3()
     test_1 = [[1,2,3],[0,2],[0,1,3],[0,2]]  # expect False
     test_2 = [[1,3],[0,2],[1,3],[0,2]]      # expect True
-    x = test_class.isBipartite(test_1, 0)
+    x = test_class.isBipartite(test_1, len(test_1))
     print(x)
-    y = test_class.isBipartite(test_2, 0)
+    y = test_class.isBipartite(test_2, len(test_2))
     print(y)
--- topologicalSort.py
+++ topologicalSort.py
@@ -1,10 +1,14 @@
 # CompSci 260P Project1, #2 by Tiffany Mejia
 # ID:  26843836
 
+# Inspritation Sources:
+# https://www.interviewcake.com/concept/java/topological-sort#:~:text=The%20topological%20sort%20algorithm%20takes,is%20called%20a%20topological%20ordering.
+# https://www.geeksforgeeks.org/topological-sorting/
 
 class Problem2:
     def __init__(self) -> None:
         self.visited = []  # Dependent on the amount of nodes
+        self.stack = []
 
 
     def topologicalSort(self, n: int, dislikes: list) -> list: 
@@ -13,8 +17,66 @@ class Problem2:
             @param n: (int) Amount of dislikes
             @param dislikes: (list[list[int]) Graph of dislikes
 
-            @return: (list) list[list[int]] graph
+            @return: (list) sorted graph
         """
-        #TODO
-        self.visited = [False] * n
-        pass
+        self.stack = []
+        self.visited = [0] * n
+
+        for node in range(n):
+            self.sort(dislikes, node)
+            if len(self.stack) == n: # already sorted
+                break
+        reversed_stack = reversed(self.stack)
+        return list(reversed_stack)
+
+    def sort(self, graph: list, node: int) -> bool:
+        """
+            Sort graph in reverse order
+            
+            @param graph: (list) list[list[int]] graph to check
+            @param n: (int) test node 
+
+            @return: (bool) True or false if node found with matching color as a connected node
+        """
+        # print(f"---{node}---")
+        neighbors = graph[node]
+        self.visited[node] = True
+
+        if neighbors == [] or neighbors is None: # condition is no directed edges
+            self.visited[node] = True
+            if node not in self.stack:
+                self.stack.append(node)
+        
+        for next_node in neighbors:
+            if not self.iscycle(graph, node, next_node):
+                if self.visited[next_node]:
+                    if next_node not in self.stack:
+                        self.stack.append(node)
+                else:
+                    self.sort(graph, next_node)
+        if node not in self.stack:
+            self.stack.append(node)
+
+    def iscycle(self, graph, node, next_node):
+        next_neighbors = graph[next_node]
+        if node in next_neighbors:
+            print("EXCEPTION: Cycle is contained in graph")
+            exit(1)
+        for node in next_neighbors:
+            if self.visited[node] == True and node not in self.stack:
+                print("EXCEPTION: Cycle is contained in graph")
+                exit(1)
+
+
+# Main used to test algorithm
+if __name__ == "__main__":
+    test_class = Problem2()
+    test_1 = [[1,2], [3, 4], [3], [4], []]          # expect [0, 2, 1, 3, 4]
+    test_2 = [[], [], [3], [1], [0,1], [0,2]]       # expect [5, 4, 2, 3, 1, 0]
+    test_3 = [[], [5], [3], [1], [0,1], [0,2]]       # expect cycle
+    x = test_class.topologicalSort(len(test_1), test_1)
+    print(x)
+    y = test_class.topologicalSort(len(test_2), test_2)
+    print(y)
+    z = test_class.topologicalSort(len(test_3), test_3)
+    print(z)
