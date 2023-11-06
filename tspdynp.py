38fd04f12d878881ddb0307189e17fc06c05f714
Tiffany <mejia9tiffany@gmail.com>
2023-11-05 21:39:04 -0800

    Add file for project 3

---
+++ tspdynp.py
@@ -0,0 +1,24 @@
+# CompSci 260P Project3 by Tiffany Mejia
+# ID:  26843836
+
+
+# You are required to implement the functions tsp_dynamic_program and costOfJourney in tspdynp.py.
+
+# For this project, you have a few requirements:
+
+#     You must implement the functions in tspdynp.py .
+#     You must use dynamic programming to solve the problem. Use the comments, to explain your approach: 
+#     Header comment: general approach, and pseudocode. Also, include inline comments related to the general comments in the header and before every function.
+#     The journey you return from tsp_dynamic_program must begin at vertex zero. Code to adjust the journey accordingly is commented out in the provided file.
+#     I will provide test cases separately. Your program must run reasonably fast.
+
+#     You may assume all inputs are valid; for example, in costOfJourney, the second parameter will always be a permutation of the unsigned ints in the range of [0, n-1], where n is the number of vertices in the given graph. You do not need to do bounds checking or the like.
+
+# If you would like to reuse some or all of your code (not that of someone else) from other projects with proper documentation for part of this project, you are welcome to do so.
+
+
+def tsp_dynamic_program():
+    pass
+
+def costOfJourney():
+    pass
